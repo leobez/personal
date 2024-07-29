773cd61576a5859e7f50f4fbfd8d5b98c42d6179
leobez <leossbezerra@gmail.com>
2024-07-29 10:35:19 -0300

    progress on curriculum section

--- components/home/Curriculum.tsx
+++ components/home/Curriculum.tsx
@@ -1,9 +1,32 @@
 export default function Curriculum() {
 
     return (
-        <section className="bg-color01 p-5 rounded-lg shadow-lg flex gap-5 h-[500px] scroll-mt-[100px]" id="curriculum">
+        <section className="bg-color01 p-5 rounded-lg shadow-lg flex flex-col gap-5 scroll-mt-[100px]" id="curriculum">
 
-            Curriculum
+            <div>
+                <p className="font-bold text-lg">Curriculum</p>
+            </div>
+            
+            <div className="flex justify-between">
+
+                <div className="h-[600px] w-9/12">
+                    <embed
+                        src="Curriculum.pdf"
+                        type="application/pdf"
+                        width="100%"
+                        height="100%"
+                        title="Embedded PDF Viewer"
+                    />
+                </div>
+
+                <div className="h-fit flex flex-col bg-color03 shadow-lg rounded-lg hover:opacity-50 duration-200 cursor-pointer">   
+                    <a href="/Curriculum.pdf" download={'Leob-CV'} className="p-5">
+                        Donwload CV
+                    </a>
+                </div>
+            </div>
+
+
 
         </section>
     )
