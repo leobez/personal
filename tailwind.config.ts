25c5f0e6b568130d252c5d7175f9ae71baed5c3d
Leo <leowsl@domain.com>
2025-07-23 16:57:31 -0300

    debug

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from "tailwindcss";
-const colors = require('tailwindcss/colors')
 
 const config: Config = {
 
@@ -8,38 +7,6 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      
-    },
-    colors: {
-      // Using modern `rgb`
-      color01: 'rgb(var(--color01) / <alpha-value>)',
-      color02: 'rgb(var(--color02) / <alpha-value>)',
-      color03: 'rgb(var(--color03) / <alpha-value>)',
-      color04: 'rgb(var(--color04) / <alpha-value>)',
-      colorText: 'rgb(var(--colorText) / <alpha-value>)',
-      ...colors
-    },
-/*     borderRadius: {
-      lg: '0',
-      sm: '0',
-      md: '0'
-    } */
-  },
-
-  safelist: [
-    /* Projects names */
-    "hover:before:content-['cinelog']",
-    "hover:before:content-['divulgacampo']",
-    "hover:before:content-['seo-comparison']",
-    "hover:before:content-['ads-content']",
   ],
 
   plugins: [
